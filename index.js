b6f6c555ea36ebcbcfdae81757f6fc467385b0c5
HypeDeveloper <de.hypedeveloper@gmail.com>
2023-04-07 16:51:48 +0100

    final update

--- index.js
+++ index.js
@@ -15,19 +15,25 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
+// serve static access
+// app.use(express.static(path.join(__dirname, './client')))
+// app.get('*', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, './','client', 'index.html'))
+// })
+
+app.use(express.static('client'))
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, 'client')});
+})
+
 
 // setup routes
 app.use("/api", require("./backend/routes"));
 
 
-// serve static access
-app.use(express.static(path.join(__dirname, './client')))
-app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, './','client', 'index.html'))
-})
-// app.use(express.static('./client'))
-
 // listen to for server
 app.listen(port, () => {
     console.log(`SERVER: listening on port ${port}`);
 })
+
+module.exports = app
